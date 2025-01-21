7e6a5152f6bf23b9cda337aa72eb8525a6574f5e
maria_zhiganova <my.nik.mariann@gmail.com>
2025-01-22 00:12:30 +0300

    fixing code by pep8

--- src/utils.py
+++ src/utils.py
@@ -4,14 +4,16 @@ from settings import JSON_PATH
 from src.category import Category
 from src.product import Product
 
+
 def read_json(file_path: str) -> dict:
     """
     Функция, которая читает json-файл: принимает путь до файла и возвращает словарь с данными
     """
-    with open(file_path, 'r', encoding='UTF-8') as f:
+    with open(file_path, "r", encoding="UTF-8") as f:
         data = json.load(f)
 
     return data
+
 
 def create_objects_from_json(data: dict):
     """
@@ -20,14 +22,15 @@ def create_objects_from_json(data: dict):
     categories = []
     for cat in data:
         products = []
-        for product in cat['products']:
+        for product in cat["products"]:
             products.append(Product(**product))
-        cat['products'] = products
+        cat["products"] = products
         categories.append(Category(**cat))
 
     return categories
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     raw_data = read_json(JSON_PATH)
     categories_data = create_objects_from_json(raw_data)
     print(categories_data)
--- tests/test_category.py
+++ tests/test_category.py
@@ -1,10 +1,7 @@
-from src.category import Category
-
-
 def test_category_init(category_phone):
-    assert category_phone.name == 'Смартфоны'
-    assert category_phone.description == 'Умные телефоны'
-    assert category_phone.products == ['iPhone 15 Pro Max', 'Huawei 7S']
+    assert category_phone.name == "Смартфоны"
+    assert category_phone.description == "Умные телефоны"
+    assert category_phone.products == ["iPhone 15 Pro Max", "Huawei 7S"]
 
     assert category_phone.category_count == 1
-    assert category_phone.product_count  == 2
+    assert category_phone.product_count == 2
