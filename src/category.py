1b54315f54be6fe86bc530606aed33e747f9b440
maria_zhiganova <my.nik.mariann@gmail.com>
2025-01-28 02:04:58 +0300

    adding add_products method to get privat attr and adding getter

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """
     Класс для представления категорий товаров, подсчета их кол-ва, а также подсчета кол-ва товаров в каждой категории
@@ -5,13 +8,28 @@ class Category:
 
     name: str
     description: str
-    products: list
+    __products: list
     category_count = 0
     product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    @property
+    def products_in_list(self):
+        return self.__products
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        prod_str = ""
+        for product in self.__products:
+            prod_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return prod_str
